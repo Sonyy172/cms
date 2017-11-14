0c07d838958e406cf2a6e9e2153986bd0c297e6e
Sonyy172 <phuc.nh94@gmail.com>
2017-11-14 10:24:27 +0700

    a

---
+++ login-callback.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/phpsdk5/autoload.php';
+ 
+session_start();
+ 
+$fb = new Facebook\Facebook([
+  'app_id' => '385750371842199',
+  'app_secret' => '4907802fbf3e614c611542fced8ad7dc',
+  'default_graph_version' => 'v2.4',
+  'default_access_token' => '4907802fbf3e614c611542fced8ad7dc'
+]);
+ 
+$helper = $fb->getRedirectLoginHelper();
+ 
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  //echo 'Graph returned an error: ' . $e->getMessage();
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  //echo 'Facebook SDK returned an error: ' . $e->getMessage();
+}
+ 
+if (isset($accessToken)) {
+  // Logged in!
+  $_SESSION['facebook_access_token'] = (string) $accessToken;
+} elseif ($helper->getError()) {
+  // The user denied the request
+}
+header('Location: index.php');
